147a09b804425e1b11ac04e28aa1cc5ed8636fef
German Sanchez <germands2013>
2022-09-11 19:11:40 -0300

    rnavbar design updated

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,15 @@
 import "./NavBar.css"
 
 function NavBar() {
+  // -----NAVBAR RETURN-----
     return (
       <nav className="navbar">
-        <a className="nav-item" href=""><p>EZbuy</p></a>
+        <a className="nav-logo" href=""><p>EZbuy</p></a>
+        <div className="burger">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
         <ul className="nav-list">
           <a className="nav-item" href=""><li>Inicio</li></a>
           <a className="nav-item" href=""><li>Servicios</li></a>
