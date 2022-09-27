4afb45d6fda88945d9aa6af6c9575256b7225d62
German Sanchez <germands2013>
2022-09-27 17:41:51 -0300

    trying to solve netlify problems part 1

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,15 +1,12 @@
 import "./CartWidget.css";
 import React from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCartShopping} from "@fortawesome/free-solid-svg-icons";
-import * as iconList from "@fortawesome/free-solid-svg-icons";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 function CartWidget() {
   // -----CART RETURN-----
-    return (
-      <FontAwesomeIcon icon={ faCartShopping } />
-    );
-  }
+  return <FontAwesomeIcon icon={faCartShopping} />;
+}
 
 export default CartWidget;
