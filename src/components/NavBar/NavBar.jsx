e7c38e2b6361d6d66d8ca361192a37fc781b5d51
German Sanchez <germands2013>
2022-10-11 00:41:19 -0300

    navbar done responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,41 +3,36 @@ import CartWidget from "../CartWidget/CartWidget";
 import { Link } from "react-router-dom";
 
 function NavBar() {
-  // ----BURGER MENU (WIP)----
-  function handleBurger(evt) {
-    let style = evt.target.style;
-    style.backgroundColor = "red";
-  }
-
   // -----NAVBAR RETURN-----
   return (
-    <nav className="navbar">
+    <div className="navbar">
       <Link className="nav-logo" to="/">
         <p>EZbuy</p>
       </Link>
-      <div onClick={handleBurger} className="burger">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <ul className="nav-list">
-        <Link className="nav-item" to="/">
-          <li>Inicio</li>
-        </Link>
-        <Link className="nav-item" to="/categorias">
-          <li>Categorías</li>
-        </Link>
-        <Link className="nav-item" to="/">
-          <li>Tienda</li>
-        </Link>
-        <Link className="nav-item" to="">
-          <li>Contacto</li>
-        </Link>
-        <Link className="nav-item" to="/cart">
-          <CartWidget />
-        </Link>
-      </ul>
-    </nav>
+      <input className="side-menu" type="checkbox" id="side-menu" />
+      <label className="hamb" for="side-menu">
+        <span className="hamb-line"></span>
+      </label>
+      <nav className="nav">
+        <ul className="menu">
+          <Link className="nav-item" to="/">
+            <li>Inicio</li>
+          </Link>
+          <Link className="nav-item" to="/categorias">
+            <li>Categorías</li>
+          </Link>
+          <Link className="nav-item" to="/">
+            <li>Tienda</li>
+          </Link>
+          <Link className="nav-item" to="">
+            <li>Contacto</li>
+          </Link>
+          <Link className="nav-item" to="/cart">
+            <CartWidget />
+          </Link>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
